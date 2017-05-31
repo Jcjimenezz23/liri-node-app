2b9d01eeebb6f878a3cff8ec69976017e789c69e
Jc <jcjimenezz23@gmail.com>
2017-05-31 16:07:02 -0400

    added files & keys

---
+++ javascript/keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'j8Ynsx4XiZBbYvNCtzQixj3Ja',
+  consumer_secret: 'qv9F2GqPQ7XsXS82d22cV7JIP5EGANjDrVexfnDgFCVLddvrBt',
+  access_token_key: '2196850853-QCvbunj4d3dU9DpDKc38axyqqdrM07DZpjeXnhV',
+  access_token_secret: 'UfFiyHxu99bRQB2phXYsrOiMSTg2HHsL7EX0DeYYxnjjv',
+}
